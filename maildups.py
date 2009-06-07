e38eb48e955c9f27611c335240e78c64f5185b55
Igor Serebryany <igor47@moomers.org>
2009-06-07 14:21:05 +0000

    seems to work

--- maildups.py
+++ maildups.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
-import sys, shutils
+import sys, shutil
 import mailbox, hashlib
 
 def main(args):
 	if len(args) != 3:
 		print "usage: %s <input file> <output file>" % args[0]
 
-	shutils.copyfile(args[1], args[2])
+	shutil.copyfile(args[1], args[2])
 
 	box = mailbox.mbox(args[2])
 	hashes = {}
+	total = 0
 	for key in box.iterkeys():
+		total += 1
 		m = hashlib.md5()
 		m.update(box.get_string(key))
 		h = m.digest()
@@ -18,10 +20,17 @@ def main(args):
 		if h not in hashes:
 			hashes[h] = []
 		hashes[h].append(key)
-	
+	print "Parsed %s messages" % total
+
+	box.lock()
+	discarded = 0
 	for h, msglist in hashes.iteritems():
 		for key in msglist[1:]:
+			discarded += 1
 			box.discard(key)
+	box.unlock()
+	box.close()
+	print "discarded %s messages" % discarded
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	main(sys.argv)
